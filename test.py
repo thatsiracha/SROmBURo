ed3d7518abe5f0be788cc601301de21d329177a6
ProtoDane <161979266+ProtoDane@users.noreply.github.com>
2026-05-26 21:57:41 -0700

    .

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ omburo = Omburo()
 print("Starting Omburo test sequence...")
 print("=" * 60)
 
-BEAR_connected = Omburo.getError()
+BEAR_connected = omburo.getError()
 if not BEAR_connected:
     print("Error: Unable to connect to BEAR. Please check connections and try again.")
     exit(1)

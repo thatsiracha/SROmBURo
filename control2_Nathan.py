0c061d427658222239f4de176eba755c6721cfb3
NateCantCode <nate7ly@g.ucla.edu>
2026-06-07 00:56:12 -0700

    control2_Nathan.py

--- control2_Nathan.py
+++ control2_Nathan.py
@@ -378,7 +378,7 @@ class Controller:
         roll_cmd = K11 * roll + K12 * rolldot + K13 * phi_rolldot + K14 * self._phi_roll_pos + KI_ROLL * self._roll_int
         roll_cmd_op = -roll_cmd
         # Axis 2, pitch: OmburoRev2 f2_goal. The roller coupling scale belongs here.
-        pitch_cmd = (K21 * pitch + K22 * pitchdot  + K24 * self._phi_pitch_pos + KI_PITCH * self._pitch_int) * N_ROLLER - (K23 * phi_pitchdot)
+        pitch_cmd = -(K21 * pitch + K22 * pitchdot  + K24 * self._phi_pitch_pos + KI_PITCH * self._pitch_int) * N_ROLLER - (K23 * phi_pitchdot)
 
         # BEAR wiring mapping: id2 = roll_cmd, id1 = pitch_cmd - roll_cmd.
         motor2_cmd = float(np.clip(roll_cmd_op, -VEL_MAX, VEL_MAX))

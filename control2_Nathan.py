9bf3d009f6c4885e53198a21018763c2a40e8647
NateCantCode <nate7ly@g.ucla.edu>
2026-06-07 00:49:13 -0700

    modify control2_Nathan.py

--- control2_Nathan.py
+++ control2_Nathan.py
@@ -343,8 +343,8 @@ class Controller:
         #   id1 + id2 = pitch axis
         motor2dot = vel_w
         motor1dot = vel_r
-        phi_rolldot = motor2dot
-        phi_pitchdot = motor2dot + motor1dot
+        phi_rolldot = motor2dot + motor1dot
+        phi_pitchdot = motor2dot 
 
         # EMA low-pass filter
         a_ang  = EMA_ALPHA_ANG
